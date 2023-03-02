0fd2f0a178cc7511506ea430bf380736c3b94db2
JarZis <mhjarjis@gmail.com>
2023-03-02 10:38:56 +0600

    Constant and variable variable

--- class_2.php
+++ class_2.php
@@ -1,1 +1,26 @@
+<?php
+//Constant
+$paid = 'PAID';
+Define('STATUS_'.$paid,'Paid');
+echo STATUS_PAID;
+echo defined(STATUS_PAID); #checking const is exist or not if exist then return boolean true
+//Another way to define constant
+const IS_ACTIVE = true;
+echo IS_ACTIVE;
+if(true){
+    //const CHECK = 'new value'; // we cannot use const in conditional statement
+    #We can use Define
+    Define('CHECK','VALUE');
+    echo CHECK;
+}
 
+//variable variable
+$foo = 'Bar';
+$$foo = 'Jarjis';
+
+echo "this is test ${$foo}";
+echo "this is test {$$foo}";
+
+//Magic Methods
+echo __FILE__;
+echo PHP_VERSION;
