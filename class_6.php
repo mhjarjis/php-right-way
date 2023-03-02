ba4e4ad8c18cddc633dbc2bac4f2337e924b0838
JarZis <mhjarjis@gmail.com>
2023-03-02 12:17:10 +0600

    String and Null

---
+++ class_6.php
@@ -0,0 +1,40 @@
+<?php
+#STRING
+
+$firstName = "Mehedi Hasan";
+$lastName = "Jarjis";
+$name = $firstName. ' '.$lastName;
+$name[1] = 'E';
+echo $name.'<br>';
+echo "Name: {$name}".'<br>';
+echo "Name: ${name}".'<br>';
+//Find String Length
+echo strlen($name).'<br>';
+//Count Word
+echo str_word_count($name).'<br>';
+//Reverse String
+echo strrev($name).'<br>';
+//Search for a text within a String
+echo strpos($name,'Jarjis').'<br>'; #it returns index number if match otherwise return false
+
+//Heredoc
+$x = 1;
+$y = 2;
+$z = 3;
+$text = <<<TEXT
+Line 1 $x
+line 2 $y
+line 3 $z
+TEXT;
+
+echo nl2br($text);
+echo '<br>';
+//Nowdoc
+$text = <<<'TEXT'
+Line 1
+Line 2
+Line 4
+TEXT;
+
+echo $text;
+
---
+++ class_7.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = null;
+var_dump((string) $x);
+
+echo is_null($x);
+if($x ===null){
+    echo 'Null';
+}
